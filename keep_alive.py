46e40b2af14f8a4a8a8ef14b530697b94b7b77a1
max462902-blip <max462902@gmail.com>
2026-01-11 20:09:52 +0530

    Remove keep_alive.py file

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,1 @@
-from flask import Flask
-from threading import Thread
 
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Hello! I am alive"
-
-def run():
-    # Render port 8080 ya default port expect karta hai
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
